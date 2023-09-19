23e86a1d19982318afff0dc5456ae8e11d10e836
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-09-18 20:32:08 -0400

    fixed rotating tiles

--- repo/js/main.js
+++ repo/js/main.js
@@ -2,39 +2,29 @@ document.getElementById('buttonReset').addEventListener('click', function () {
     location.reload();
 });
 
-var playerOne = 'X'; // Create the global variables for the players
+var currentPlayerSymbol = 'X';
+var playerOne = 'X';
 var playerTwo = 'O';
-let playerOneWin = 0; // score starts at zero
+let playerOneWin = 0;
 let playerTwoWin = 0;
 let stalemateWin = 0;
-let movesMade = 0;
 
 function attachTableToButton() {
-    document.getElementById("app").appendChild(createBOARD()); // took the app div and appended the createBOARD function to it
+    document.getElementById("app").appendChild(createBOARD());
 }
-// which is what makes the tictactoe board and resets the table
-// function attachTableToButton() {
-//     var appTable = createBOARD();
-//     var appDiv = document.getElementById("app");         //<<<<< much more convoluted way of doing this from weather app project
-//     appDiv.appendChild(appTable);
-// }
 
 const button = document.getElementById("chooseTile");
 
-document.getElementById("characterInput").value = "TURN X"; //makes starting turn in the input box
+document.getElementById("characterInput").value = "TURN X";
 
 function currentPlayer() {
-    var characterInput = document.getElementById("characterInput");
-    if (characterInput.value === "TURN X") {
-        characterInput.value = "TURN O";
-    } else {
-        characterInput.value = "TURN X";
-    }
+    currentPlayerSymbol = (currentPlayerSymbol === 'X') ? 'O' : 'X';
+    document.getElementById("characterInput").value = `TURN ${currentPlayerSymbol}`;
 }
 
-function updateScore(){
+function updateScore() {
     var score = document.getElementById("scoreText");
-    score.textContent = `Player X Wins ${playerOneWin}| Player O Wins ${playerTwoWin}| Stalemates ${stalemateWin} `;
+    score.textContent = `Player X Wins ${playerOneWin}| Player O Wins ${playerTwoWin}| Stalemates ${stalemateWin}`;
 }
 
 function createBOARD() {
@@ -42,7 +32,7 @@ function createBOARD() {
     board.classList.add("table", "table-bordered");
 
     var rows = [
-        ['1', '2', '3'], // nested arrays allow me to control them all without having to have row variables for each
+        ['1', '2', '3'],
         ['4', '5', '6'],
         ['7', '8', '9']
     ];
@@ -50,62 +40,38 @@ function createBOARD() {
     rows.forEach(function (row) {
         var tableRow = document.createElement("tr");
 
-        function changeXCell(cell, content) {
-            cell.textContent = content;
-        }
-
-        function changeOCell(cell, content) { // the content is tied only to the content within the function? so why isnt it working?
-            cell.textContent = content;
-        }
-
         row.forEach(function (value) {
-            var cell = document.createElement("td"); // got rid of the .maps that extrapolate to each string literal
+            var cell = document.createElement("td");
             cell.classList.add("d-table-cell", "p-5", "bd-highlight");
 
-            var buttonX = document.createElement("button");
-            buttonX.classList.add("btn", "btn-primary", "choose-tile");
-            buttonX.textContent = "X";
-            buttonX.setAttribute("data-selected", "true");
-            buttonX.addEventListener("click", function () {
-                changeXCell(cell, buttonX.textContent);
-                movesMade++;
-                if (checkForWin(playerOne)) {
-                    playerOneWin++;
-                    updateScore();
-                    alert("Player X VICTORY! SUCK IT PLAYER O!");
-                } else if (movesMade === 9) { // Check for stalemate when all moves are made
-                    stalemateWin++;
-                    updateScore();
-                    alert("Stalemate!");
-                } else {
+            var button = document.createElement("button");
+            button.classList.add("btn", "btn-primary", "choose-tile");
+            button.textContent = "";
+            button.setAttribute("data-selected", "false");
+            button.addEventListener("click", function () {
+                if (button.getAttribute("data-selected") === "false") {
+                    button.textContent = currentPlayerSymbol;
+                    button.setAttribute("data-selected", "true");
+
+                    if (checkForWin(currentPlayerSymbol)) {
+                        if (currentPlayerSymbol === 'X') {
+                            playerOneWin++;
+                            alert("Player X VICTORY! SUCK IT PLAYER O!");
+                        } else {
+                            playerTwoWin++;
+                            alert("Player O VICTORY! SUCK IT PLAYER X!")
+                        }
+                    } else if (checkStalemate()) {
+                        stalemateWin++;
+                    }
+
                     currentPlayer();
                 }
             });
 
-            var buttonO = document.createElement("button");
-            buttonO.classList.add("btn", "btn-primary", "choose-tile");
-            buttonO.textContent = "O";
-            buttonO.setAttribute("data-selected", "true");
-            buttonO.addEventListener("click", function () {
-                changeOCell(cell, buttonO.textContent);
-                movesMade++;
-                if (checkForWin(playerTwo)) {
-                    playerTwoWin++;
-                    updateScore();
-                    alert("Player O VICTORY! SUCK IT PLAYER X!");
-                } else if (movesMade === 9) { // Check for stalemate when all moves are made
-                    stalemateWin++;
-                    updateScore();
-                    alert("Stalemate!");
-                } else {
-                    currentPlayer();
-                }
-            });
-
-            cell.appendChild(buttonX);
-            cell.appendChild(buttonO);
+            cell.appendChild(button);
             cell.insertAdjacentHTML("beforeend", `<span class="btn-text">${value}</span>`);
-            tableRow.appendChild(cell); // beforeend => Just inside the element, after its last child.
+            tableRow.appendChild(cell);
         });
         board.appendChild(tableRow);
     });
@@ -126,11 +92,12 @@ var winningCombinations = [
 ];
 
 function checkForWin(player) {
-    var cells = document.querySelectorAll("td");
+    var cells = document.querySelectorAll("td button");
 
     for (var i = 0; i < winningCombinations.length; i++) {
         var [a, b, c] = winningCombinations[i];
         if (cells[a].textContent === player && cells[b].textContent === player && cells[c].textContent === player) {
+            updateScore();
             return true;
         }
     }
@@ -138,42 +105,13 @@ function checkForWin(player) {
 }
 
 function checkStalemate() {
-    var cells = document.querySelectorAll("td");
+    var cells = document.querySelectorAll("td button");
     for (var i = 0; i < cells.length; i++) {
-        if (cells[i].textContent === "") { // if an empty cell is found, game isnt over
+        if (cells[i].textContent === "") {
             return false;
         }
     }
+    updateScore();
     return true;
 }
-// function playGame(){                        //Overarching function for how the game functions
-//         function ticTacToeStatus(){        //How it checks whether three in a row is achieved or stalemate(THE PART I NEED TO FIGURE OUT)
-//             if
-//             return checkForWin()
-//         }
 
-//         function checkForWin(){            //Response for a win or stalement
-//             IF (threeInRow) = (window.alert("`${winner}` WINS!!!!")):
-//                 {resetBoard;tallyScore};
-
-//             IF (boardIsFull) = (window.alert("STALEMATE! RESETTING BOARD...")):
-//                 {resetBoard;tallyScore};
-//         }
-
-//         function tallyScore(winner) {       //Scorecard tracker
-//             if (winner === "playerOne") {
-//                 playerOneWin++;
-//             }
-//             else if (winner === "playerTwo"){
-//                 playerTwoWin++;
-//             }
-//             else if (winner === "staleMate"){
-//                 stalemateWin++;
-//             }
-//         }
-
-//         function resetBoard() {             //Reset button or autoreset
-
-//             return createBoard();
-//         }
-// }
